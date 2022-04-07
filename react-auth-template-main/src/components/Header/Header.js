96e3097632c836091ed201f59e13aad19e999966
Maxwell Kwarteng <maxtheflash007@gmail.com>
2022-04-06 20:17:53 -0400

    Added all the modified files to the Chanson Music Studios Website

--- react-auth-template-main/src/components/Header/Header.js
+++ react-auth-template-main/src/components/Header/Header.js
@@ -13,7 +13,12 @@ const authenticatedOptions = (
 const unauthenticatedOptions = (
   <Fragment>
     <NavLink to='/sign-up' className='nav-link'>Sign Up</NavLink>
-    <NavLink to='/sign-in' className='nav-link'>Sign In</NavLink>
+    <NavLink to='/about' className='nav-link'>About</NavLink>
+    <NavLink to='/lessons' className='nav-link'>Lessons</NavLink>
+    <NavLink to='/our staff' className='nav-link'>Our Staff</NavLink>
+    <NavLink to='/blog' className='nav-link'>Blog</NavLink>
+    <NavLink to='/contact' className='nav-link'>Contact</NavLink>
+    <NavLink to='/faqs' className='nav-link'>FAQ&#39;s</NavLink>
   </Fragment>
 )
 
@@ -26,7 +31,7 @@ const alwaysOptions = (
 const Header = ({ user }) => (
   <Navbar bg='primary' variant='dark' expand='md'>
     <Navbar.Brand>
-      <Link to='/' style={{ color: '#FFF', textDecoration: 'none' }}>react-auth-template</Link>
+      <Link to='/' style={{ color: '#F9F8F4', textDecoration: 'none' }} className='nav-link'>Chanson Music Studios</Link>
     </Navbar.Brand>
     <Navbar.Toggle aria-controls='basic-navbar-nav' />
     <Navbar.Collapse id='basic-navbar-nav'>
